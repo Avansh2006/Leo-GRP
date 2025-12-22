6f9f2e711004f07e69bacf7f0ad56dc370787e4f
Avansh2006 <avanshyadav20072006@gmail.com>
2025-12-22 14:48:08 +0530

    f

--- app/bodycam-commands/page.tsx
+++ app/bodycam-commands/page.tsx
@@ -285,7 +285,7 @@ export default function BodycamCommandsPage() {
         <div>
           <h1 className="text-4xl font-bold text-blue-400 mb-2">{selectedOrg} Bodycam Commands</h1>
           <p className="text-gray-400">
-            Made by Carlos Bloomfield (115452)
+            Made by Avansh Yadav
           </p>
         </div>
         
